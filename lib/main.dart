2d2bf7bb099387c5a2dbdf59a8eb806feeb68171
Komi Rodrigue KOUDAKPO <gabirusamaa@gmail.com>
2025-07-08 19:51:22 +0000

    Initialisation Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
-import 'package:dukoavote/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'src/src.dart';
 
-void main() {
-  runApp(const DukoaVoteApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EnvConfig.load();
+  EnvConfig.validate();
+  await Supabase.initialize(
+    url: EnvConfig.supabaseUrl,
+    anonKey: EnvConfig.supabaseAnonKey,
+  );
+  runApp(
+    const ProviderScope(
+      child: DukoaVoteApp(),
+    ),
+  );
 }
 
